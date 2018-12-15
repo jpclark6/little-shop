9d0bae25dd7c8d5dc4a42f13eacebf7ae7adddcd
Maddie Jones <maddyg91@gmail.com>
2018-12-15 13:19:20 -0700

    Added validation on relationship test for user

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+ validates_presence_of :email, :name, :role, :password, :address, :city, :state, :zip_code
+ validates_uniqueness_of :email
+
+ has_many :orders
+ has_many :items
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :email}
+    it {should validate_uniqueness_of :email}
+    it {should validate_presence_of :role}
+    it {should validate_presence_of :password}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :state}
+    it {should validate_presence_of :zip_code}
+  end
+  describe 'relationships' do
+    it {should have_many :items}
+    it {should have_many :orders}
+  end
+end
