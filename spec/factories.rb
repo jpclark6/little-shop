32388d4d17e18900c0f24754b32dbeb3f9ace743
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-16 15:16:32 -0700

    Adds css selector.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,13 +2,13 @@ FactoryBot.define do
   factory :user do
     name {"Lisa"}
     sequence(:email) { |n| "Lisa#{n}@gmail.com" }
-    address "123 Happy St"
-    city "Denver"
-    zip_code 80015
-    state 'CO'
-    password '2738^f'
-    role 0
-    enabled true
+    address {"123 Happy St"}
+    city { "Denver" }
+    zip_code { 80015 }
+    state { 'CO' }
+    password { '2738^f' }
+    role { 0 }
+    enabled { true }
     factory :merchant do
       role 1
     end
@@ -18,11 +18,13 @@ FactoryBot.define do
   end
 
   factory :item do
-    name 'Thing 1'
-    instock_qty 27
-    price 44
-    image 'fgkfgkjd'
-    description 'this thing is....'
-    enabled true
+    association :user, factory: :merchant
+    # merchant
+    sequence(:name) { |n| "Thing #{n}" }
+    instock_qty { 27 }
+    price { 44 }
+    image { 'fgkfgkjd' }
+    description { 'this thing is....' }
+    enabled { true }
   end
 end
--- spec/features/user_can_see_catalog_spec.rb
+++ spec/features/user_can_see_catalog_spec.rb
@@ -8,14 +8,13 @@ describe 'As any type of user' do
     image = 'fgkfgkjd'
     description = 'this thing is....'
 
-    binding.pry
-    visit items_path
-
     item_1 = FactoryBot.create(:item, name: name, instock_qty: instock_qty, price: price, image: image, description: description)
+    visit items_path
+    save_and_open_page
     within "#item-#{item_1.id}" do
 
       expect(page).to have_content(item_1.name)
-      expect(page).to have_content(item_1.merchant.name)
+      expect(page).to have_content(item_1.user.name)
       expect(page).to have_content(item_1.instock_qty)
       expect(page).to have_content(item_1.price)
       expect(page).to have_css("img[src='#{item_1.image}']")
