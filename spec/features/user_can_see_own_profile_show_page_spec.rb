9a658bf309df19fade274fe71ce7e3b4c26ddbba
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-16 11:26:41 -0700

    Adds testing for user info appearing on user show page

--- spec/features/user_can_see_own_profile_show_page_spec.rb
+++ spec/features/user_can_see_own_profile_show_page_spec.rb
@@ -2,11 +2,25 @@ require "rails_helper"
 
 describe 'user visits their own profile page' do
   it 'shows all profile data except password' do
-  User.create(name: 'Jill', address: '378 S Broadway', city: 'Denver', state: 'CO', zip_code: 80015 )
+    name = 'Jill'
+    address = '122 Broadway'
+    email = '123@gmail.com'
+    password = '337*7!'
+    city = 'Denver'
+    state = 'CO'
+    zip_code = 80015
+
+    Factorybot.build_stubbed(:user, name: name, address: address, email: email, password: password, city: city, state: state, zip_code: zip_code )
 
     visit user_path(user)
 
-
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user.address)
+    expect(page).to have_content(user.email)
+    expect(page).to_not have_content(user.password)
+    expect(page).to have_content(user.city)
+    expect(page).to have_content(user.state)
+    expect(page).to have_content(user.zip_code)
   end
 
 end
