46cc60e782b154865123f5ebb7e206a9e8f00c47
Maddie Jones <maddyg91@gmail.com>
2018-12-16 11:03:54 -0700

    Routes added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ require 'simplecov'
 SimpleCov.start
 RSpec.configure do |config|
 
-  config.include FactoryBot::Syntax::Methods
+  # config.include FactoryBot::Syntax::Methods
 
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
