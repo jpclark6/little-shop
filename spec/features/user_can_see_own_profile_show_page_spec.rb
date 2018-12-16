fc5b65aa81e5fb320bdfddfba45cdf739286cea5
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-16 11:05:49 -0700

    Adds updated diagram to read.me. Begin feature testing.

---
+++ spec/features/user_can_see_own_profile_show_page_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe 'user visits their own profile page' do
+  it 'shows all profile data except password' do
+    User.create(name: 'Jill', address: '378 S Broadway', city: 'Denver', state: 'CO', zip_code: 80015, enabled: )
+
+    visit user_path(user)
+
+
+  end
+
+end
