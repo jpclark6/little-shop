8387bc60007675066e8ada4b4b598c4d6899960f
jpclark6 <jpclark6@gmail.com>
2018-12-17 16:18:42 -0700

    Users cannot log in with bad credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}"
+      if user.registered?
+        redirect_to profile_path(user)
+      elsif user.merchant?
+        redirect_to "/dashboard/#{user.id}"
+      elsif user.admin?
+        redirect_to root_path
+      end
+    else
+      flash[:failure] = "Invalid credentials."
+      render :new
+    end
+  end
 end
