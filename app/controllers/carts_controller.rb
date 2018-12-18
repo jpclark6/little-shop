49c4d871d969f8b9019e49518663449fac0fe713
Maddie Jones <maddyg91@gmail.com>
2018-12-17 20:25:59 -0700

    Feature test for adding multiple items to cart and getting the correct falsh message written and passing

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,14 @@
 class CartsController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def create
-    
     item = Item.find(params[:item_id])
-    flash[:notice] = "You now have #{item.name} in your cart."
+    item_id_str = item.id.to_s
+    session[:cart] ||= Hash.new(0)
+    session[:cart][item_id_str] ||= 0
+    session[:cart][item_id_str] = session[:cart][item_id_str] + 1
+    quantity = session[:cart][item_id_str]
+    flash[:notice] = "You now have #{pluralize(quantity, "copy")} of #{item.name} in your cart."
     redirect_to items_path
   end
 end
--- spec/features/user_can_add_item_to_their_cart_spec.rb
+++ spec/features/user_can_add_item_to_their_cart_spec.rb
@@ -11,4 +11,18 @@ describe "when user adds songs to their cart" do
 
     expect(page).to have_content("You now have #{item.name} in your cart")
   end
+  it 'the message correctly increments for multiple songs' do
+    user = FactoryBot.build_stubbed(:user)
+    item = FactoryBot.create(:item)
+
+    visit items_path
+
+    click_button "Add Item"
+
+    expect(page).to have_content("You now have 1 copy of #{item.name} in your cart")
+
+    click_button "Add Item"
+
+    expect(page).to have_content("You now have 2 copies of #{item.name} in your cart")
+  end
 end
