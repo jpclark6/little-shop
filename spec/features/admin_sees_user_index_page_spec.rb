2b4a1446bfae2dd9b9a80a0b597753f5540b1e3f
Ben Lee <bendelonlee@gmail.com>
2018-12-18 17:13:33 -0700

    Implements seeing register date next to each user

--- spec/features/admin_sees_user_index_page_spec.rb
+++ spec/features/admin_sees_user_index_page_spec.rb
@@ -5,7 +5,6 @@ describe 'user index page' do
     it "shows all registered users" do
       user_1 = FactoryBot.create(:user)
       user_2 = FactoryBot.create(:user, enabled: false)
-
       merchant = FactoryBot.create(:merchant)
       admin = FactoryBot.create(:admin)
 
@@ -15,13 +14,18 @@ describe 'user index page' do
       within ".nav-link-container" do
         click_on 'Users'
       end
-      save_and_open_page
+
       expect(current_path).to eq("/admin/users")
       within "#user-#{user_1.id}" do
         expect(page).to have_content(user_1.name)
+        expect(page).to have_content("Registered at #{user_1.created_at.to_date}")
+        expect(page).to have_button("Disable")
+
       end
       within "#user-#{user_2.id}" do
         expect(page).to have_content(user_2.name)
+        expect(page).to have_content("Registered at #{user_2.created_at.to_date}")
+        expect(page).to have_button("Enable")
       end
 
       expect(page).to_not have_css("#user-#{merchant.id}")
@@ -42,7 +46,5 @@ describe 'user index page' do
 end
 
 
-# Each user's name is a link to a show page for that user ("/admin/users/5")
-# Next to each user's name is the date they registered
 # I see a "disable" button next to any users who are not yet disabled
 # I see an "enable" button next to any users whose accounts are disabled %>
