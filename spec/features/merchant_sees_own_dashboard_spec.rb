61ed14852277548a5c1342dcd91a17e6ee9ce2f4
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-19 13:58:03 -0700

    Adds tests setup for factories, visits, and several expectations. Tests are not fully written yet.

--- spec/features/merchant_sees_own_dashboard_spec.rb
+++ spec/features/merchant_sees_own_dashboard_spec.rb
@@ -23,10 +23,62 @@ describe 'As a Merchant' do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit "/dashboard"
+    visit dashboard_path
 
     click_on 'View Items'
 
-    expect(current_path).to eq('/dashboard/items')
+    expect(current_path).to eq(dashboard_merchant_items_path)
+  end
+
+  it 'If any users have pending orders containing items I sell Then I see a list of these orders' do
+    # Current_user
+    user = FactoryBot.create(:merchant)
+
+    # Another merchant
+    user_2 = FactoryBot.create(:merchant)
+
+    # Item for other merchant
+    item_1 = FactoryBot.create(:item, user: user_2)
+
+    # Item for current_user
+    item_2 = FactoryBot.create(:item, user: user)
+
+    # Order - pending -for other merchant - should not show up
+    order_1 = FactoryBot.create(:pending, item: item_1)
+
+    # Order - pending -for current_user - should show up
+    order_2 = FactoryBot.create(:pending)
+    order_item = FactoryBot.create(:order_item, order: order_2, item: item_2)
+
+    # Order - cancelled -for current_user - should not show up
+    order_3 = FactoryBot.create(:cancelled, item: [item_1, item_2])
+
+    # Order - fulfilled -for current_user - should not show up
+    order_4 = FactoryBot.create(:fulfilled, item: [item_1, item_2])
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_merchant_items_path
+
+    expect(page).to have_link(order_2.id)
+    expect(page).to have_content(order_2.created_at)
+    expect(page).to have_content(order_item.quantity)
+    expect(page).to have_content(order_item.)
+    expect(page).to 
+
+
   end
 end
+
+
+# User Story 39
+# Merchant Dashboard displays Orders
+#
+# As a merchant
+# When I visit my dashboard ("/dashboard")
+# If any users have pending orders containing items I sell Then I see a list of these orders.
+# Each order listed includes the following information:
+# - the ID of the order, which is a link to the order show page ("/dashboard/orders/15")
+# - the date the order was made
+# - the total quantity of my items in the order
+# - the total value of my items for that order
