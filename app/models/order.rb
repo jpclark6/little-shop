327b5b400bbf74a0f0f4e79087723941ceac5309
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-19 18:27:51 -0700

    Integrates total_quanity method or orders.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,8 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :items, through: :order_items
   enum status: ["pending", "fulfilled", "cancelled"]
+
+  def total_quantity
+    order_items.sum(:quantity)
+  end
 end
