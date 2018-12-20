864ea135b2ea51aa4a81521e6700013cb2c51a8c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-19 18:28:11 -0700

    Tests total_quanity method

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,4 +8,21 @@ RSpec.describe Order, type: :model do
     it {should belong_to :user}
     it {should have_many :order_items}
   end
+
+  describe 'instance method' do
+    it '.total_quantity' do
+
+      item_1 = FactoryBot.create(:item)
+      item_2 = FactoryBot.create(:item)
+      item_3 = FactoryBot.create(:item)
+
+      order = FactoryBot.create(:pending, items: [item_1,item_2,item_3])
+
+      item_1.order_items.first.update(quantity: 1)
+      item_2.order_items.first.update(quantity: 2)
+      item_3.order_items.first.update(quantity: 3)
+
+      expect(order.total_quantity).to eq(6)
+    end
+  end
 end
