21d29921b35123f65f0e8eee04c253f28d290ec3
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-19 19:32:24 -0700

    Adds total_price model test.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe Order, type: :model do
 
   describe 'instance method' do
     it '.total_quantity' do
-
       item_1 = FactoryBot.create(:item)
       item_2 = FactoryBot.create(:item)
       item_3 = FactoryBot.create(:item)
@@ -23,6 +22,18 @@ RSpec.describe Order, type: :model do
       item_3.order_items.first.update(quantity: 3)
 
       expect(order.total_quantity).to eq(6)
+    end
+
+    it '.total_price' do
+      item_1 = FactoryBot.create(:item)
+      item_2 = FactoryBot.create(:item)
+      item_3 = FactoryBot.create(:item)
+
+      order = FactoryBot.create(:pending, items: [item_1,item_2,item_3])
+
+      item_1.order_items.first.update(quantity: 1, price: 5)
+      item_2.order_items.first.update(quantity: 2)
+      item_3.order_items.first.update(quantity: 3)
     end
   end
 end
