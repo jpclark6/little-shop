ed7e0402f317697f1dcf81ef89f00edcb284d198
jpclark6 <jpclark6@gmail.com>
2018-12-20 11:23:50 -0700

    fixes dashboard item test

--- config/routes.rb
+++ config/routes.rb
@@ -27,12 +27,11 @@ Rails.application.routes.draw do
   namespace :dashboard do
     get "", to: 'users#show'
     get "/items", to: 'items#index'
-    get "/items/edit/:id", to: "item#edit", as: "item_edit"
+    get "/items/edit/:id", to: "items#edit", as: "item_edit"
     get "/orders", to: 'orders#index'
     get "/orders/:id", to: "orders#show"
-    delete "/items/delete/:id", to: "item#destroy", as: "item"
-    patch "/items/toggle/:id", to: "item#toggle", as: "item_toggle"
-
+    delete "/items/delete/:id", to: "items#destroy", as: "item"
+    patch "/items/toggle/:id", to: "items#toggle", as: "item_toggle"
   end
 
   get '/register', to: 'users#new'
