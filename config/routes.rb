7c306b8a30892da272d3c8ff9c9ff79ee08a3b9b
jpclark6 <jpclark6@gmail.com>
2018-12-20 19:55:12 -0700

    fixes css on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
   resources :carts, only: [:create]
   resources :users, only: [:index, :create, :edit]
 
-
   resources :orders, only: [:show, :destroy, :index, :create] do
     resources :order_items, only: [:update]
   end
