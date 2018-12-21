9916f4a42a5d1f1352b2d0d0d217c40a06c277a0
Ben Lee <bendelonlee@gmail.com>
2018-12-21 09:20:53 -0700

    Adds migration to change Order table's default to 0 / pending

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181217195631) do
+ActiveRecord::Schema.define(version: 20181221161446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20181217195631) do
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
-    t.integer "status"
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
