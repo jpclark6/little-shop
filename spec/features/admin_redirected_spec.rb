0f819234f59f89f0e17042f95c723d812550dbfd
Ben Lee <bendelonlee@gmail.com>
2018-12-22 14:47:19 -0700

    Completes user story 44

--- spec/features/admin_redirected_spec.rb
+++ spec/features/admin_redirected_spec.rb
@@ -1,21 +1,22 @@
 require "rails_helper"
 describe 'as an admin' do
-  it 'I am redirected from user show to merchant dashboard' do
+  before(:each) do
     admin = FactoryBot.create(:admin)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  end
+  it 'I am redirected from user show to merchant dashboard' do
 
     merchant = FactoryBot.create(:merchant)
-
     visit "/admin/users/#{merchant.id}"
 
-
     expect(current_path).to eq(admin_merchant_path(merchant))
+  end
+  it 'I am redirected from merchant dashboard to user show' do
 
-#     As an admin user
-# If I visit a profile page for a user, but that user is a merchant
-# Then I am redirected to the appropriate merchant dashboard page.
-# eg, if I visit "/admin/users/7" but that user is a merchant
-# Then I am redirected to "/admin/merchants/7"
-# And I see their merchant dashboard page
+    user = FactoryBot.create(:user)
+    visit "/admin/merchants/#{user.id}"
+
+    expect(current_path).to eq(admin_user_path(user))
+
   end
 end
