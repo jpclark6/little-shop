862273cabf9ea56734a8dbfb972c281b751069e4
Ben Lee <bendelonlee@gmail.com>
2018-12-21 18:16:28 -0700

    Order show page works

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
+  # validates_presence_of :price, :quantity
+  # These would ideally be here (along with tests). Breaks Rspec for now
+  # Would need to be accompanied by perhaps a before_validation method on the model
+
   belongs_to :item
   belongs_to :order
 
--- spec/features/merchant_sees_an_order_show_page_spec.rb
+++ spec/features/merchant_sees_an_order_show_page_spec.rb
@@ -37,10 +37,30 @@ describe 'order show page' do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
 
       visit dashboard_order_path(order)
-      save_and_open_page
       expect(page).to have_css("#item-#{item_1.id}")
       expect(page).to_not have_css("#item-#{item_2.id}")
       expect(page).to_not have_css("#item-#{item_3.id}")
+    end
+    it "shows each item's information" do
+      merchant = FactoryBot.create(:merchant)
+      item_1 = FactoryBot.create(:item)
+      item_2 = FactoryBot.create(:item)
+      merchant.items += [item_1, item_2]
+
+      order = FactoryBot.create(:order)
+
+      order_item_1 = FactoryBot.create(:order_item, item: item_1, order: order, price: 3, quantity: 1.50)
+      order_item_2 = FactoryBot.create(:order_item, item: item_2, order: order, price: 2.75, quantity: 10)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+      visit dashboard_order_path(order)
+      within "#item-#{item_1.id}" do
+        expect(page).to have_link(item_1.name, href: item_path(item_1))
+        expect(page).to have_content(item_1.description)
+        expect(page).to have_css("img[src='#{item_1.image}']")
+        expect(page).to have_content("Quantity: #{order_item_1.quantity}")
+        expect(page).to have_content("Price: $#{order_item_1.price}")
+        expect(page).to have_content("Subtotal: $#{order_item_1.subtotal}")
+      end
     end
   end
 
