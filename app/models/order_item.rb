8947f42b38bfa96ad8f18fb438f0b2c5a1b42213
Ben Lee <bendelonlee@gmail.com>
2018-12-22 09:50:25 -0700

    Adds before validations to order_item model so weird factory bot data doesn't break test

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,10 @@ class OrderItem < ApplicationRecord
   # validates_presence_of :price, :quantity
   # These would ideally be here (along with tests). Breaks Rspec for now
   # Would need to be accompanied by perhaps a before_validation method on the model
+  before_validation :ensure_quantity, :ensure_price
+
+
+
 
   belongs_to :item
   belongs_to :order
@@ -9,4 +13,16 @@ class OrderItem < ApplicationRecord
   def subtotal
     quantity * price
   end
+
+  private
+
+  def ensure_quantity
+    self.quantity ||= 1
+  end
+
+  def ensure_price
+    self.price ||= 1
+  end
+
+
 end
