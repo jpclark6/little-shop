6e1892a2cfb8282d13dc2fb5e1868a69d4f4663e
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-27 11:06:17 -0800

    Alters test

--- app/controllers/dashboard/order_items_controller.rb
+++ app/controllers/dashboard/order_items_controller.rb
@@ -3,8 +3,9 @@ class Dashboard::OrderItemsController < ApplicationController
 
   def fulfill
     order_item = OrderItem.find(params[:id])
-    if order_item.pending?
+    if order_item.fulfilled?
       order_item.update(fulfilled: true)
+      binding.pry
       redirect_to dashboard_order_path(order)
     end
   end
