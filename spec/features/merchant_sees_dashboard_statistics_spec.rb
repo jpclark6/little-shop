25524fa5894e4a11fde1f62a887f0f04c336cf2f
jpclark6 <jpclark6@gmail.com>
2018-12-27 13:39:25 -0700

    Merchant dashboard statistics complete

--- spec/features/merchant_sees_dashboard_statistics_spec.rb
+++ spec/features/merchant_sees_dashboard_statistics_spec.rb
@@ -17,7 +17,7 @@ describe 'as a merchant on my dashboard' do
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_1_order_1)
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_1_order_2)
 # $1000, 200 pcs
-    @user_2 = FactoryBot.create(:user, city: 'Denver', state: 'MO')
+    @user_2 = FactoryBot.create(:user, city: 'Denver', state: 'MI')
     @user_2_order_1 = FactoryBot.create(:fulfilled, user: @user_2)
     @user_2_order_2 = FactoryBot.create(:fulfilled, user: @user_2)
     @user_2_order_3 = FactoryBot.create(:fulfilled, user: @user_2)
@@ -59,19 +59,15 @@ describe 'as a merchant on my dashboard' do
   it 'can find top 5 items sold by quantity' do
     visit dashboard_path
     within('.statistics') do
-      expected = "Top items sold by quantity: #{@item_1.name}, #{@item_2.name}, #{@item_3.name}, #{@item_4.name}, & #{@item_5.name}"
+      expected = "Top items sold by quantity: #{@item_1.name}, #{@item_2.name}, #{@item_4.name}, #{@item_3.name}, & #{@item_6.name}"
       expect(page).to have_content(expected)
     end
-    # @item_1, @item_2, @item_4, @item_3, @item_6
   end
   it 'can find percentage sold against total inventory' do
     visit dashboard_path
-    within('.statistics') do
-      within(".item-#{@item_1}") do
-        expect(page).to have_content("Sold 500 items, which is 50% of your total inventory")
-      end
+    within(".statistics") do
+      expect(page).to have_content("Sold 859 items, which is 35% of your total inventory")
     end
-    # @item_1 - 50%
   end
   it 'can find top 3 states where items were shipped' do
     visit dashboard_path
@@ -83,7 +79,7 @@ describe 'as a merchant on my dashboard' do
   it 'can find top 3 city/states where items were shipped' do
     visit dashboard_path
     within('.statistics') do
-      expect(page).to have_content("Top 3 city/states by quantity shipped: Denver, MO; Salt Lake City, UT; & Denver, CO")
+      expect(page).to have_content("Top 3 city/states by quantity shipped: Denver, MI; Salt Lake City, UT; & Denver, CO")
     end
     # Denver, MO, Salt Lake City, UT, Denver, CO
   end
@@ -109,13 +105,3 @@ describe 'as a merchant on my dashboard' do
     # @user_5, @user_3, @user_2
   end
 end
-
-
-# When I visit my dashboard, I see an area with statistics:
-# - top 5 items I have sold by quantity
-# - total quantity of items I've sold, and as a percentage against my sold units plus remaining inventory (eg, if I have sold 1,000 things and still have 9,000 things in inventory, the message would say something like "Sold 1,000 items, which is 10% of your total inventory")
-# - top 3 states where my items were shipped
-# - top 3 city/state where my items were shipped (Springfield, MI should not be grouped with Springfield, CO)
-# - name of the user with the most orders from me (pick one if there's a tie)
-# - name of the user who bought the most total items from me (pick one if there's a tie)
-# - top 3 users who have spent the most money on my items
