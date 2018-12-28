ed9d943902ac9ac9d108f2918358332f7bc8cb93
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-12-28 11:47:25 -0800

    Removes changes to revert master to normal

--- spec/features/merchant_sees_an_order_show_page_spec.rb
+++ spec/features/merchant_sees_an_order_show_page_spec.rb
@@ -124,29 +124,5 @@ describe 'order show page' do
       expected_quantity = item_1.instock_qty - order_item_1.quantity
       expect(page).to have_content("Inventory: #{expected_quantity}")
     end
-
-    it 'Shows a big red notice next to the item indicating I cannot fulfill this item' do
-      merchant = FactoryBot.create(:merchant)
-      item_1 = FactoryBot.create(:item)
-      item_2 = FactoryBot.create(:item)
-      merchant.items += [item_1, item_2]
-      order = FactoryBot.create(:order)
-
-      order_item_1 = FactoryBot.create(:order_item, item: item_1, order: order, price: 3, quantity: 1.50)
-      order_item_2 = FactoryBot.create(:order_item, item: item_2, order: order, price: 2.75, quantity: 10)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
-
-      visit dashboard_order_path(order)
-
-      # within "#item-#{order_item_1.item_id}" do
-      #   expect(page).to have_content("Status: Fulfilled")
-      # end
-
-      # within "#item-#{item_2.id}" do
-      #   click_on 'Fulfill'
-      # end
-      #
-    end
   end
 end
