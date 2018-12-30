778cbb51f12d54c3a50488496b3e67b9c0aa3edb
jpclark6 <jpclark6@gmail.com>
2018-12-29 20:00:17 -0700

    refactors merchant stats

--- spec/features/merchant_sees_dashboard_statistics_spec.rb
+++ spec/features/merchant_sees_dashboard_statistics_spec.rb
@@ -16,7 +16,7 @@ describe 'as a merchant on my dashboard' do
     @user_1_order_2 = FactoryBot.create(:fulfilled, user: @user_1)
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_1_order_1)
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_1_order_2)
-# $1000, 200 pcs
+
     @user_2 = FactoryBot.create(:user, city: 'Denver', state: 'MI')
     @user_2_order_1 = FactoryBot.create(:fulfilled, user: @user_2)
     @user_2_order_2 = FactoryBot.create(:fulfilled, user: @user_2)
@@ -24,7 +24,6 @@ describe 'as a merchant on my dashboard' do
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_2_order_1)
     FactoryBot.create(:order_item, item: @item_2, price: @item_2.price, quantity: 105, fulfilled: true, order: @user_2_order_2)
     FactoryBot.create(:order_item, item: @item_3, price: @item_3.price, quantity: 50, fulfilled: true, order: @user_2_order_3)
-# $1550, 255 pcs,
 
     @user_3 = FactoryBot.create(:user, city: 'Salt Lake City', state: 'UT')
     @user_3_order_1 = FactoryBot.create(:fulfilled, user: @user_3)
@@ -33,33 +32,30 @@ describe 'as a merchant on my dashboard' do
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_3_order_1)
     FactoryBot.create(:order_item, item: @item_4, price: @item_4.price, quantity: 50, fulfilled: true, order: @user_3_order_2)
     FactoryBot.create(:order_item, item: @item_4, price: @item_4.price, quantity: 51, fulfilled: true, order: @user_3_order_3)
-# $4540, 201 pcs
 
     @user_4 = FactoryBot.create(:user, city: 'Los Angeles', state: 'CA')
     @user_4_order_1 = FactoryBot.create(:fulfilled, user: @user_4)
     @user_4_order_2 = FactoryBot.create(:fulfilled, user: @user_4)
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_4_order_1)
     FactoryBot.create(:order_item, item: @item_5, price: @item_5.price, quantity: 1, fulfilled: true, order: @user_4_order_2)
-# $580, 101 pcs
 
     @user_5 = FactoryBot.create(:user, city: 'Fort Collins', state: 'CO')
     @user_5_order_1 = FactoryBot.create(:fulfilled, user: @user_5)
     @user_5_order_2 = FactoryBot.create(:fulfilled, user: @user_5)
     FactoryBot.create(:order_item, item: @item_6, price: @item_6.price, quantity: 2, fulfilled: true, order: @user_5_order_1)
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_5_order_2)
-# $100,500, 102 pcs
 
     @user_6 = FactoryBot.create(:user, city: 'Dallas', state: 'TX')
     @user_6_order_1 = FactoryBot.create(:pending, user: @user_6)
     FactoryBot.create(:order_item, item: @item_6, price: @item_6.price, quantity: 2, fulfilled: false, order: @user_6_order_1)
-# 0
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
   end
   it 'can find top 5 items sold by quantity' do
     visit dashboard_path
     within('.statistics') do
-      expected = "Top items sold by quantity:\n1. #{@item_1.name}\n2. #{@item_2.name}\n3. #{@item_4.name}\n4. #{@item_3.name}\n5. #{@item_6.name}"
+      expected = "Top items sold by quantity:\n#{@item_1.name} #{@item_2.name} #{@item_4.name} #{@item_3.name} #{@item_6.name}"
       expect(page).to have_content(expected)
     end
   end
@@ -72,37 +68,32 @@ describe 'as a merchant on my dashboard' do
   it 'can find top 3 states where items were shipped' do
     visit dashboard_path
     within('.statistics') do
-      expect(page).to have_content("Top states shipped to by items shipped:\n1. CO\n2. MI\n3. UT")
+      expect(page).to have_content("Top states shipped to by items shipped:\nCO MI UT")
     end
-    # CO, MI, UT
   end
   it 'can find top 3 city/states where items were shipped' do
     visit dashboard_path
     within('.statistics') do
-      expect(page).to have_content("Top city/states shipped to by items shipped:\n1. Denver, MI\n2. Salt Lake City, UT\n3. Denver, CO")
+      expect(page).to have_content("Top city/states shipped to by items shipped:\nDenver, MI Salt Lake City, UT Denver, CO")
     end
-    # Denver, MO, Salt Lake City, UT, Denver, CO
   end
   it 'can find name of user with most orders' do
     visit dashboard_path
     within('.statistics') do
       expect(page).to have_content("Top customer by most orders:\n#{@user_2.name}")
     end
-    # @user_2 or @user_3 # choose one for tie
   end
   it 'can find name of user who bought most total items' do
     visit dashboard_path
     within('.statistics') do
       expect(page).to have_content("Top customer by most items ordered:\n#{@user_2.name}")
     end
-    # @user_2 # choose one for tie
   end
   it 'can find top 3 users who have spent the most money on items' do
     visit dashboard_path
     within('.statistics') do
-      expect(page).to have_content("Top customers by total revenue:\n1. #{@user_5.name}\n2. #{@user_3.name}\n3. #{@user_2.name}")
+      expect(page).to have_content("Top customers by total revenue:\n#{@user_5.name} #{@user_3.name} #{@user_2.name}")
     end
-    # @user_5, @user_3, @user_2
   end
 end
 describe 'as a merchant on my dashboard with not enough data' do
@@ -118,7 +109,7 @@ describe 'as a merchant on my dashboard with not enough data' do
     @user_1_order_2 = FactoryBot.create(:fulfilled, user: @user_1)
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_1_order_1)
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_1_order_2)
-# $1000, 200 pcs
+
     @user_2 = FactoryBot.create(:user, city: 'Denver', state: 'MI')
     @user_2_order_1 = FactoryBot.create(:fulfilled, user: @user_2)
     @user_2_order_2 = FactoryBot.create(:fulfilled, user: @user_2)
@@ -126,7 +117,6 @@ describe 'as a merchant on my dashboard with not enough data' do
     FactoryBot.create(:order_item, item: @item_1, price: @item_1.price, quantity: 100, fulfilled: true, order: @user_2_order_1)
     FactoryBot.create(:order_item, item: @item_2, price: @item_2.price, quantity: 105, fulfilled: true, order: @user_2_order_2)
     FactoryBot.create(:order_item, item: @item_3, price: @item_3.price, quantity: 50, fulfilled: true, order: @user_2_order_3)
-# $1550, 255 pcs,
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
@@ -134,7 +124,7 @@ describe 'as a merchant on my dashboard with not enough data' do
   it 'can find top 5 items sold by quantity' do
     visit dashboard_path
     within('.statistics') do
-      expected = "Top items sold by quantity:\n1. #{@item_1.name}\n2. #{@item_2.name}\n3. #{@item_3.name}"
+      expected = "Top items sold by quantity:\n#{@item_1.name} #{@item_2.name} #{@item_3.name}"
       expect(page).to have_content(expected)
     end
   end
@@ -147,36 +137,31 @@ describe 'as a merchant on my dashboard with not enough data' do
   it 'can find top 3 states where items were shipped' do
     visit dashboard_path
     within('.statistics') do
-      expect(page).to have_content("Top states shipped to by items shipped:\n1. MI\n2. CO")
+      expect(page).to have_content("Top states shipped to by items shipped:\nMI CO")
     end
-    # CO, MI, UT
   end
   it 'can find top 3 city/states where items were shipped' do
     visit dashboard_path
     within('.statistics') do
-      expect(page).to have_content("Top city/states shipped to by items shipped:\n1. Denver, MI\n2. Denver, CO")
+      expect(page).to have_content("Top city/states shipped to by items shipped:\nDenver, MI Denver, CO")
     end
-    # Denver, MO, Salt Lake City, UT, Denver, CO
   end
   it 'can find name of user with most orders' do
     visit dashboard_path
     within('.statistics') do
       expect(page).to have_content("Top customer by most orders:\n#{@user_2.name}")
     end
-    # @user_2 or @user_3 # choose one for tie
   end
   it 'can find name of user who bought most total items' do
     visit dashboard_path
     within('.statistics') do
       expect(page).to have_content("Top customer by most items ordered:\n#{@user_2.name}")
     end
-    # @user_2 # choose one for tie
   end
   it 'can find top 3 users who have spent the most money on items' do
     visit dashboard_path
     within('.statistics') do
-      expect(page).to have_content("Top customers by total revenue:\n1. #{@user_2.name}\n2. #{@user_1.name}")
+      expect(page).to have_content("Top customers by total revenue:\n#{@user_2.name} #{@user_1.name}")
     end
-    # @user_5, @user_3, @user_2
   end
 end
