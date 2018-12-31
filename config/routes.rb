b52776556be7a8f84dc688c9ecfe2e6e4b5726f9
Ben Lee <bendelonlee@gmail.com>
2018-12-31 14:19:04 -0700

    Reorganizes routes, from most public to least public

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,37 @@
 Rails.application.routes.draw do
   root 'items#index'
+
+  get '/register', to: 'users#new'
+  get '/merchants', to: 'users#index'
+  get '/merchants/:id', to: 'users#show'
+
+  get '/cart', to: 'carts#show', as: 'cart'
+  patch '/cart', to: 'carts#update'
+  delete '/cart', to: 'carts#delete'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :items, only: [:show, :index]
+  resources :carts, only: [:create]
+  resources :users, only: [:index, :create, :edit]
+
+  namespace :profile do
+    get '', to: 'users#show'
+    resources :orders, only: [:show, :destroy, :index, :create]
+    get '/edit', to: 'users#edit', as: "edit"
+    patch '/update', to: 'users#update'
+  end
+
+  namespace :dashboard do
+    get "", to: 'users#show'
+    resources :items, except: [:show]
+    resources :orders, only: [:show, :index]
+    patch "/items/toggle/:id", to: "items#toggle", as: "item_toggle"
+    patch "/order_items/fulfill/:id", to: "order_items#fulfill", as: "order_item_fulfill"
+  end
+
   namespace :admin do
     resources :merchants, only: [:none], shallow: true do
       resources :items, except: [:show]
@@ -13,37 +45,6 @@ Rails.application.routes.draw do
     get 'merchants/:id', to: "users#merchant_show", as: "merchant"
     patch "/order_items/fulfill/:id", to: "order_items#fulfill", as: "order_item_fulfill"
   end
-
-  resources :items, only: [:show, :index]
-  resources :carts, only: [:create]
-  resources :users, only: [:index, :create, :edit]
-
-  namespace :profile do
-    get '', to: 'users#show'
-    resources :orders, only: [:show, :destroy, :index, :create]
-    get '/edit', to: 'users#edit', as: "edit"
-    patch '/update', to: 'users#update'
-  end
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  get '/cart', to: 'carts#show', as: 'cart'
-  patch '/cart', to: 'carts#update'
-  delete '/cart', to: 'carts#delete'
-
-  namespace :dashboard do
-    get "", to: 'users#show'
-    resources :items, except: [:show]
-    resources :orders, only: [:show, :index]
-    patch "/items/toggle/:id", to: "items#toggle", as: "item_toggle"
-    patch "/order_items/fulfill/:id", to: "order_items#fulfill", as: "order_item_fulfill"
-  end
-
-  get '/register', to: 'users#new'
-  get '/merchants', to: 'users#index'
-  get '/merchants/:id', to: 'users#show'
 
   match '*a', :to => 'errors#not_found', via: :get
 
