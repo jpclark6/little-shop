59f517f64423f55f593ddb7d8ad86b7e0a2c3e30
Ben Lee <bendelonlee@gmail.com>
2018-12-31 12:20:26 -0700

    Makes view drier, adds path instance methods to the controllers

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -5,5 +5,7 @@ class Dashboard::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @user = @order.user
     @order_items = current_user.my_order_items(@order)
+    @order_item_fulfill_path = :dashboard_order_item_fulfill_path
+    
   end
 end
